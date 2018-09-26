6c4bd135896aaf70b5baf411f9349fa34ee4d921
Chris <chrisneilbrown@gmail.com>
2018-09-27 09:55:52 +1000

    code clean up

--- Dev/Adrian/Inv_Screen.gd
+++ Dev/Adrian/Inv_Screen.gd
@@ -21,14 +21,14 @@ func _ready():
 	
 
 func show_inventory(weapon, armour, wearable):
-	var i = 0
+	var i = 0 #not needed
 	for j in weapon.inventory:
 		var item = item_label.duplicate()
 		inv_panel.add_child(item)
-		item.get_node("Text").text = weapon.inventory[i].Name
+		item.get_node("Text").text = j.Name # j = weapon.inventory[i]
 		item.get_node("Text").hint_tooltip = weapon.inventory[i].Name
 		item.show()
-		i += 1
+		i += 1 #not needed
 	
 	i = 0
 	for j in armour.inventory:
