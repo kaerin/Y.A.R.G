0b969e48fa6b73598d12c585f538cf8c472b8977
Chris <chrisneilbrown@gmail.com>
2018-09-27 09:53:21 +1000

    Revert "code clean up"
    This reverts commit 63460686a547d3ea4e69004b81766096eeb9c1fe.

--- Dev/Adrian/Inv_Screen.gd
+++ Dev/Adrian/Inv_Screen.gd
@@ -22,10 +22,10 @@ func _ready():
 
 func show_inventory(weapon, armour, wearable):
 	var i = 0
-	for j in weapon.inventory: # j = weapon.inventory[0] etc
+	for j in weapon.inventory:
 		var item = item_label.duplicate()
 		inv_panel.add_child(item)
-		item.get_node("Text").text = j.Name # This is the same
+		item.get_node("Text").text = weapon.inventory[i].Name
 		item.get_node("Text").hint_tooltip = weapon.inventory[i].Name
 		item.show()
 		i += 1
