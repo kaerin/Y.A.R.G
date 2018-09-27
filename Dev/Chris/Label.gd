586bbf04bb7df057e10e3fecec4cdace57b1dffa
Chris <chrisneilbrown@gmail.com>
2018-09-27 11:24:39 +1000

    map gen speed testing
    changes to test map speed and success rate

--- Dev/Chris/Label.gd
+++ Dev/Chris/Label.gd
@@ -1,42 +1,48 @@
 extends Label
 
-var map = []
-var height = 70
-var width = 140
-var factor = 3 #higher means less blocks removed
+var map
+var height = 50
+var width = 100
+var factor = 2.4 #higher means less blocks removed
 var start = Vector2()
 var end = Vector2()
 var delay = .01
-var delay2 = 1
-var goodMap = "Bad"
+var goodMap = false
 
 var dirs = [Vector2(1,0),Vector2(0,1),Vector2(-1,0),Vector2(0,-1)]
 
 func _ready():
-	randomize()
-	for i in height:
-		map.append([])
-		for j in width:
-			map[i].append([])
-			map[i][j] = "+"
+	maps()
 	
-	show()
-	yield(get_tree().create_timer(delay2),"timeout")
+func maps():
+	var maps = 0
+	var mapsGood = 0
+	while true:
+		goodMap = false
+		var try = 0
+		while goodMap == false:
+			try += 1
+			randomize()
+			map = map_gen()
+			map = map_gen_start_end(map)
+			map = map_clear(map)
+			map_check(map)
+		var time = OS.get_ticks_msec()
+		maps += try
+		mapsGood += 1
+		var rate = int(float(mapsGood) / maps * 100)
+		var avg_time = time / maps
+		show(map, "Map is " + str(goodMap) + " maps: " + str(maps) + " Avg:" + str(avg_time) + "ms Rate:" + str(rate) + "%")
+		yield(get_tree().create_timer(0.001), "timeout")
+
+func map_check(map):
+	var path = []
+	path.append(start)
+	while path.size() > 0:
+		path = findNeigh(path)
+	return goodMap
 	
-	start.x = randi() % height
-	start.y = randi() % width
-	end = start
-	while abs(end.x - start.x) < height * 0.4:
-		end.x = randi() % height
-	end.y = randi() % width
-	while abs(end.y - start.y) < width * 0.4:
-		end.y = randi() % width
-	map[start.x][start.y] = "S"
-	map[end.x][end.y] = "E"
-	
-	show()
-	yield(get_tree().create_timer(delay2),"timeout")
-	
+func map_clear(map):
 	for i in range(int(height * width / factor)):
 		var clear = Vector2()
 		clear.x = randi() % height
@@ -45,19 +51,35 @@ func _ready():
 			clear.x = randi() % height
 			clear.y = randi() % width
 		map[clear.x][clear.y] = " "
+	return map
+		
+func map_gen_start_end(map):
+	start.x = randi() % height
+	start.y = randi() % width
+	end = start
+		
+	while abs(end.x - start.x) < height * 0.45:
+		end.x = randi() % height
+	while abs(end.y - start.y) < width * 0.45:
+		end.y = randi() % width
+#
+	map[start.x][start.y] = "S"
+	map[end.x][end.y] = "E"
+	return map
 	
-	show()
-	yield(get_tree().create_timer(delay2),"timeout")
-
-	var path = []
-	path.append(start)
-	while path.size() > 0:
-		path = findNeigh(path)
-		show()
-		yield(get_tree().create_timer(delay),"timeout")
+func map_gen():
+	if height < 10:
+		height = 10
+	if width < 10:
+		width = 10
+	var map = []
+	for i in height:
+		map.append([])
+		for j in width:
+			map[i].append([])
+			map[i][j] = "+"
+	return map
 	
-	show("Map is " + goodMap)
-
 func findNeigh(path):
 	var newPath = []
 	for i in path:
@@ -75,11 +97,11 @@ func findNeigh(path):
 				map[check.x][check.y] = "X"
 				newPath.append(check)
 			elif map[check.x][check.y] == "E":
-				goodMap = "Good"
+				goodMap = true
 				return []
 	return newPath
 
-func show(text = ""):
+func show(map, text = ""):
 	var maptext = text
 	for i in height:
 		maptext += "\n"
