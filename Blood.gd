f3bcc9e8a006d82f11f2fb12458b4a665ec13849
Chris <chrisneilbrown@gmail.com>
2018-09-30 18:22:44 +1000

    random blood

--- Blood.gd
+++ Blood.gd
@@ -1,5 +1,8 @@
 extends TileMap
 
+var BLOOD = ["Blood1","Blood2","Blood3","Blood4","Blood5","Blood6","Blood7","Blood8"]
+
 func set_blood(pos):
+	var t = tile_set.find_tile_by_name(BLOOD[randi() % BLOOD.size()])
 	print("setting blood ", pos)
-	set_cellv(pos,4)
+	set_cellv(pos,t)
