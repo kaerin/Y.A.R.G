a5903701547c8d400b5ee4bc8644776aa2f8f31b
Adrian <35069419+kaerin@users.noreply.github.com>
2019-02-14 21:12:04 +0100

    more cleaning

--- Player/Effects.gd
+++ Player/Effects.gd
@@ -11,16 +11,15 @@ onready var Bleeding = preload("res://Animations/Effects/Healing/Bleeding.tscn")
 #############################
 
 func blood_splatter(direction, dmg): 
-	set_blood_splatter(direction, dmg)
-	rpc('set_blood_splatter', direction, dmg, get_parent().is_in_group('Player'))	#<---- functional, but maybe theres a better way
+	set_blood_splatter(direction, dmg, true)
+	rpc('set_blood_splatter', direction, dmg)
 
-remote func set_blood_splatter(direction, dmg , is_player = false):
-	var node = get_obj_root(is_player)
-	var j = Blood.instance()	# <--- maybe consider instancing to grid_map, so is not deleted by early enemy death.
+remote func set_blood_splatter(direction, dmg, is_master=false):
+	var node = get_obj_root(is_master)
+	var j = Blood.instance()
 	j.direction = direction
 	j.particle_count = dmg	
-#	if node.is_in_group("Enemy"):
-	j.position = node.position	# <--- Not sure why this is necessary
+	j.position = node.position
 	add_effect(node, j, true)
 	
 #############################
@@ -28,11 +27,11 @@ remote func set_blood_splatter(direction, dmg , is_player = false):
 #############################	
 	
 func dmg_counter(dmg):
-	set_dmg_counter(dmg)
-	rpc('set_dmg_counter', dmg, get_parent().is_in_group('Player')) #<---- functional, but maybe theres a better way
+	set_dmg_counter(dmg, true)
+	rpc('set_dmg_counter', dmg)
 	
-remote func set_dmg_counter(dmg, is_player = false):
-	var node = get_obj_root(is_player)
+remote func set_dmg_counter(dmg, is_master=false):
+	var node = get_obj_root(is_master)
 	var j = Dmg_Counter.instance()
 	j.dmg = dmg
 	add_effect(node, j)
@@ -41,34 +40,24 @@ remote func set_dmg_counter(dmg, is_player = false):
 ## effect for HP (damage or healing etc)
 #############################
 
-func healing(i):
-	var node = get_obj_root(get_parent().is_in_group('Player'), false)	
-	var j = Healing.instance()
-	j.duration = i.get_duration()
-	j.cycle = i.get_cycle()
-	j.amount_ = i.get_amount()
-	add_effect(node, j)
-	rpc('set_healing', j.duration, j.cycle, j.amount, get_parent().is_in_group('Player')) #<---- functional, but maybe theres a better way
+func healing(j):
+	set_healing(j.duration, j.cycle, j.amount, true)
+	rpc('set_healing', j.duration, j.cycle, j.amount)
 
-remote func set_healing(i_duration, i_cycles, i_amount, is_player=false):
-	var node = get_obj_root(is_player, true)
+remote func set_healing(i_duration, i_cycles, i_amount, is_master=false):
+	var node = get_obj_root(is_master)
 	var j = Healing.instance()
 	j.duration = i_duration
 	j.cycle = i_cycles
 	j.amount_ = i_amount
 	add_effect(node, j)
 	
-func bleeding(i):
-	var node = get_obj_root(get_parent().is_in_group('Player'), false)	
-	var j = Bleeding.instance()
-	j.duration = i.get_duration()
-	j.cycle = i.get_cycle()
-	j.amount_ = i.get_amount()
-	add_effect(node, j)
-	rpc('set_bleeding', j.duration, j.cycle, j.amount, get_parent().is_in_group('Player')) #<---- functional, but maybe theres a better way
+func bleeding(j):
+	set_bleeding(j.duration, j.cycle, j.amount, true)
+	rpc('set_bleeding', j.duration, j.cycle, j.amount)
 	
-remote func set_bleeding(i_duration, i_cycles, i_amount, is_player=false):
-	var node = get_obj_root(is_player, true)
+remote func set_bleeding(i_duration, i_cycles, i_amount, is_master=false):
+	var node = get_obj_root(is_master)
 	var j = Bleeding.instance()
 	j.duration = i_duration
 	j.cycle = i_cycles
@@ -79,21 +68,18 @@ remote func set_bleeding(i_duration, i_cycles, i_amount, is_player=false):
 # Repetative functions
 ############################
 
-func get_obj_root(is_player, is_client=true):
+func get_obj_root(is_master=true):
 	var node = get_parent()
-	if is_player && is_client:
+	if node.is_in_group('Player') && not(is_master):
 		var id = get_tree().get_rpc_sender_id()
 		node = get_node('../..').get_node(str(id))	
 	return node
 
-func add_effect(node, j, effect_after_death=false):		#will expand "effect after death later"
-#	if node.is_in_group("Enemy"):
+func add_effect(node, j, effect_after_death=false):
 	if effect_after_death == true:
 		node.get_node("../..").add_child(j)  # <--- adds to grid map. doesnt delete by death. (eg. blood splatter)
 	else:
-		node.get_node("Effects").add_child(j)
-#	else:	
-#		node.get_node("Effects").add_child(j)
+		node.get_node("Effects").add_child(j)	# <--- adds to object, deleted by death (eg. spell effect)
 
 
 	
